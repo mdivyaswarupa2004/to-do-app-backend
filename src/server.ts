6c9d0d73bb61ed0d1f298b34146a8ac979c03c28
mdivyaswarupa2004 <divyaswarupa@everest.engineering>
2026-01-04 17:11:13 +0530

    Move server and app files into src folder

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import dotenv from "dotenv";
+import app from "./app";
+dotenv.config();
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`server running on https://localhost:${PORT}`);
+});
